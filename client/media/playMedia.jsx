e348e80672e3546944f5104e7365d7434ad9f2f4
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-14 19:20:00 +0700

    Customizing the media player

--- client/media/playMedia.jsx
+++ client/media/playMedia.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import { listRelated, read } from './api-media';
 import Media from './media';
 import RelatedMedia from './relatedMedia';
@@ -21,6 +23,7 @@ function PlayMedia(props) {
   const classes = useStyles();
   const [media, setMedia] = useState({ postedBy: {} });
   const [relatedMedia, setRelatedMedia] = useState([]);
+  const [autoPlay, setAutoPlay] = useState(false);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -60,15 +63,63 @@ function PlayMedia(props) {
     };
   }, [props.match.params.mediaId]);
 
+  const handleChange = (event) => {
+    setAutoPlay(event.target.checked);
+  };
+
+  const handleAutoplay = (updateMediaControls) => {
+    const playList = relatedMedia;
+    const playMedia = playList[0];
+    if (!autoPlay || playList.length == 0) return updateMediaControls();
+
+    if (playList.length > 1) {
+      playList.shift();
+      setMedia(playMedia);
+      setRelatedMedia(playList);
+    } else {
+      listRelated({ mediaId: playMedia._id }).then(
+        (data) => {
+          if (data.error) {
+            console.log(data.error);
+          } else {
+            setMedia(playMedia);
+            setRelatedMedia(data);
+          }
+        },
+      );
+    }
+  };
+
+  const nextUrl = relatedMedia.length > 0
+    ? `/media/${relatedMedia[0]._id}`
+    : '';
+
   return (
     <div className={classes.root}>
       <Grid container spacing={8}>
         <Grid item xs={8} sm={8}>
-          <Media media={media} />
+          <Media
+            media={media}
+            nextUrl={nextUrl}
+            handleAutoplay={handleAutoplay}
+          />
         </Grid>
 
         {relatedMedia.length > 0 && (
           <Grid item xs={4} sm={4}>
+            <FormControlLabel
+              className={classes.toggle}
+              control={(
+                <Switch
+                  checked={autoPlay}
+                  onChange={handleChange}
+                  color="primary"
+                />
+              )}
+              label={
+                autoPlay ? 'Autoplay ON' : 'Autoplay OFF'
+              }
+            />
             <RelatedMedia media={relatedMedia} />
           </Grid>
         )}
